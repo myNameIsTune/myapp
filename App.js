04355dededb1f2b4f575a8ee5bc60bfc303bc01b
Tune <cfdw011@gmail.com>
2020-08-07 16:15:17 +0900

    install react-navigation

--- App.js
+++ App.js
@@ -1,30 +1,35 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
-import {default as theme}from './custom-theme.json';
+import { ApplicationProvider, IconRegistry, Layout, Text,Button,Icon } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {default as theme} from './custom-theme.json';
 import {default as mapping} from './mapping.json';
+import { NavigationContainer } from '@react-navigation/native';
 
-const HomeScreen = () => (
+const HomeIcon = (props) => (
+  <Icon name='home-outline' {...props} />
+);
+const LoginButton = () => (
+  <Button accessoryLeft={HomeIcon}>Login with Facebook</Button>
+  );
+  
+  const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Button category='h1'>HOME</Button>
-    <Text category="h1">Hi my name is TaeYun how about you?</Text>
-    <Text category="s1">Hi my name is TaeYun how about you?</Text>
-    <Text category="p1">Hi my name is TaeYun how about you?</Text>
-    <Text category="c1">Hi my name is TaeYun how about you?</Text>
-   </Layout>
+    <Button accessoryLeft={HomeIcon} category='h1'>HOME</Button>
+  </Layout>
 );
 
 export default () => (
-  <ApplicationProvider 
-  {...eva} 
-  theme={{...eva.light, ...theme}}
-  customMapping={mapping}>
-    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Button category='h1'>HOME</Button>
-    <Text category="h1">Hi my name is TaeYun how about you?</Text>
-    <Text category="s1">Hi my name is TaeYun how about you?</Text>
-    <Text category="p1">Hi my name is TaeYun how about you?</Text>
-    <Text category="c1">Hi my name is TaeYun how about you?</Text>
-   </Layout>
-  </ApplicationProvider>
+  <NavigationContainer>
+
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{...eva.dark,...theme}} customMapping={mapping}>
+
+     <HomeScreen />
+
+    </ApplicationProvider>
+
+  </NavigationContainer>
+
 );
